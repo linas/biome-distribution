8fded32e08767d9fea1a012824224bac9d7e14ca
Linas Vepstas <linasvepstas@gmail.com>
2020-01-05 01:42:20 +0000

    Working version

--- inst.scm
+++ inst.scm
@@ -1,13 +1,17 @@
 
 ; Maintain counts
-(define accum-time
-	(let ((elapsed 0)
+(define (accum-time name)
+	(let ((fname name)
+			(elapsed 0)
 			(calls 0)
 			(start 0))
 		(lambda (enter? report?)
 			(if report?
-				(begin
-				)
+				(format #t "Time: ~6f secs. calls: ~A avg: ~2f usec/call for ~A\n"
+					(* 1.0e-9 elapsed)
+					calls
+					(/ (* 1.0e-3 elapsed) calls)
+					fname)
 				(if enter?
 					(set! start (get-internal-real-time))
 					(begin
