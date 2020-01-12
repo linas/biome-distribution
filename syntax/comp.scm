caa7d4b9c75a56fbfeb5bacab07ea2a0fba6dc28
Linas Vepstas <linasvepstas@gmail.com>
2020-01-12 16:08:34 +0000

    stuff

--- syntax/comp.scm
+++ syntax/comp.scm
@@ -1,18 +1,28 @@
 
+
+(define incr
+	(let ((ctr 0))
+		(lambda ()
+			(set! ctr (+ 1 ctr))
+			ctr)))
+
 (define-syntax display-compile-timestamp
   (lambda (x)
     (syntax-case x ()
       ((_)
        #`(begin
           (display "The compile timestamp was: ")
-          (display #,(current-time))
+          (display #,(incr))
           (newline))))))
-
-
-(define fooj (current-time))
-(define fook (display-compile-timestamp))
-(define (foom) (display-compile-timestamp))
 
 (define (bar x)
    (format #t "Called bar with ~A\n" x)
    (+ x 1))
+
+
+
+; (define fooj (current-time))
+(define fook (display-compile-timestamp))
+(define fokk (display-compile-timestamp))
+(define (foom) (display-compile-timestamp))
+
