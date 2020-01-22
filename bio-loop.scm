0fb8b81561a1a4cc4615b344e1c0b34a2d82a953
Linas Vepstas <linasvepstas@gmail.com>
2020-01-22 13:56:38 +0000

    Hacking the benchmark.

--- bio-loop.scm
+++ bio-loop.scm
@@ -1,11 +1,11 @@
 ;
 ; bio-loop.scm
+; Modified copy of the benchmark.
 ;
 (use-modules (srfi srfi-1))
 (use-modules (opencog) (opencog exec))
 
 ; Needeed for definition of GeneNode and MoleculeNode
-; You can skip this by redefining these two, below.
 (use-modules (opencog bioscience))
 
 ; Performance stats timer
@@ -16,44 +16,6 @@
 			(define diff (/ (- now start-time) internal-time-units-per-second))
 			(set! start-time now)
 			diff)))
-
-; List of genes on which to perform the query.
-(load "gene-list.scm")
-
-; Non-human-readable definitions are used to compress the size
-; of the data file to something reasonable-ish.
-;
-; The `(opencog bioscience)` module defines GeneNode and MoleculeNode
-; but if that is not available, just redefine it to be ConceptNode.
-; (define Gene Concept)
-(define e Evaluation)
-(define l List)
-(define g Gene)
-(define m Molecule)
-(define c Concept)
-(define s Member)
-(define i (Predicate "interacts_with"))
-(define x (Predicate "expresses"))
-(define b (Predicate "has_biogridID"))
-(define d (Predicate "has_pubmed_ID"))
-(define z (Predicate "has_entrez_id"))
-
-(format #t "Start loading raw data ...\n")
-(define elapsed-secs (make-timer))
-; (primitive-load "biogrid.scm")
-(primitive-load "biogrid-full.scm")
-(format #t "Loaded biogrid data in ~6f seconds\n" (elapsed-secs))
-(primitive-load "entrez.scm")
-(format #t "Loaded entrez data in ~6f seconds\n" (elapsed-secs))
-(primitive-load "uniprot.scm")
-(format #t "Loaded uniprot data in ~6f seconds\n" (elapsed-secs))
-(primitive-load "reactome-chebi.scm")
-(format #t "Loaded chebi reactome in ~6f seconds\n" (elapsed-secs))
-(primitive-load "reactome-ncbi.scm")
-(format #t "Loaded ncbi reactome in ~6f seconds\n" (elapsed-secs))
-(primitive-load "reactome-uniprot.scm")
-(format #t "Loaded uniprot reactome in ~6f seconds\n" (elapsed-secs))
-(format #t "AtomSpace contents: ~A\n" (cog-report-counts))
 
 ; ------------------------------------------------------------------
 ; Define the main benchmarking routine
@@ -110,7 +72,7 @@
 
 ;; -----------
 ;; Run the triangle-benchmark
-(define (run-triangle-benchmark)
+(define (run-triangle-benchmark gene-list)
 	(define bench-secs (make-timer))
 	(define interaction-counts
 		(map
@@ -125,7 +87,7 @@
 				(define rlen (cog-arity result))
 
 				; Collect up some stats
-				; (cog-inc-count! gene rlen)
+				(cog-inc-count! gene rlen)
 				(for-each
 					(lambda (gene-pair)
 						(define gene-a (cog-outgoing-atom gene-pair 0))
@@ -201,7 +163,7 @@
 ;; -----------
 ;; The long pentagon-benchmark. This takes several hours.
 ;;
-(define (run-long-pentagon-benchmark)
+(define (run-long-pentagon-benchmark gene-list)
 
 	; Create a list of the pathways that the genes are in.
 	(define pathways (delete-duplicates
@@ -233,26 +195,9 @@
 	(pentagon-benchmark (map Concept paths))
 )
 
-; Run the benchmark three times
-(display "Will run the triangle benchmark three times ...\n")
-(run-triangle-benchmark)
-(sleep 1)
-(run-triangle-benchmark)
-(sleep 1)
-(run-triangle-benchmark)
-(sleep 1)
-(display "Will run the short pentagon benchmark three times ...\n")
-(run-short-pentagon-benchmark)
-(sleep 1)
-(run-short-pentagon-benchmark)
-(sleep 1)
-(run-short-pentagon-benchmark)
+; (define elapsed-secs (make-timer))
+(format #t "AtomSpace contents: ~A\n" (cog-report-counts))
 
-(display "Will run the long pentagon benchmark once ...\n")
-(display "This takes hours...")
-(run-long-pentagon-benchmark)
-
-(exit)
 
 ; =================================================================
 
