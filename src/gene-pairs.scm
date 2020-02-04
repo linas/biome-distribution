4b7942e25e686a5ffd83a7198ce4d7e54e42ca3d
Linas Vepstas <linasvepstas@gmail.com>
2020-02-04 21:50:33 +0000

    Simplify the other one, too.

--- src/gene-pairs.scm
+++ src/gene-pairs.scm
@@ -83,123 +83,13 @@
        ListLink
           GeneNode \"SIRT1\"
           AnyNode \"right-gene\"
-
-  Finally, the 'left-type and 'right-type methods return the type
-  of the the two sides of the pair.
 "
-	(let ((all-pairs '()))
-
-		; Get the observational count on ATOM.
-		(define (get-count ATOM) (cog-count ATOM))
-
-		(define any-left (AnyNode "left-gene"))
-		(define any-right (AnyNode "right-gene"))
-		(define gene-pair-pred (PredicateNode "interacts_with"))
-
-		(define (get-left-type) 'GeneNode)
-		(define (get-right-type) 'GeneNode)
-		(define (get-pair-type) 'EvaluationLink)
-
-		; Return the atom holding the count, if it exists, else
-		; return nil.
-		(define (get-pair L-ATOM R-ATOM)
-			(define maybe-list (cog-link 'ListLink L-ATOM R-ATOM))
-			(if (null? maybe-list) '()
-				(cog-link 'EvaluationLink gene-pair-pred maybe-list)))
-
-		; Create an atom to hold the count (if it doesn't exist already).
-		(define (make-pair L-ATOM R-ATOM)
-			(EvaluationLink gene-pair-pred (List L-ATOM R-ATOM)))
-
-		; Return the left member of the pair. Given the pair-atom,
-		; locate the left-side atom.
-		(define (get-left-element PAIR)
-			(gadr PAIR))
-		(define (get-right-element PAIR)
-			(gddr PAIR))
-
-		; Return the raw observational count on PAIR. If the counter for
-		; PAIR does not exist (was not observed), then return 0.
-		(define (get-pair-count L-ATOM R-ATOM)
-			(define pr (get-pair L-ATOM R-ATOM))
-			(if (null? pr) 0 (get-count pr)))
-
-		; Caution: this unconditionally creates the wildcard pair!
-		(define (get-left-wildcard WORD)
-			(make-pair any-left WORD))
-
-		; Caution: this unconditionally creates the wildcard pair!
-		(define (get-right-wildcard WORD)
-			(make-pair WORD any-right))
-
-		(define (get-wild-wild)
-			(make-pair any-left any-right))
-
-		; get-all-pairs - return a list holding all of the observed
-		; gene-pairs.  Caution: this can be tens of millions long!
-		(define (do-get-all-pairs)
-			; The list of pairs is mostly just the incoming set of the
-			; predicate node. However, this does include some junk, sooo ...
-			; hey, both left and right better be genes.
-		   (filter!
-				(lambda (pair)
-					(and
-						(equal? 'GeneNode (cog-type (gadr pair)))
-						(equal? 'GeneNode (cog-type (gddr pair)))))
-				(cog-incoming-by-type gene-pair-pred 'EvaluationLink)))
-
-		(define (get-all-pairs)
-			(if (null? all-pairs) (set! all-pairs (do-get-all-pairs)))
-			all-pairs)
-
-		; fetch-gene-pairs -- fetch all counts for gene pairs
-		; from the currently-open database.
-		(define (fetch-gene-pairs)
-			(define start-time (current-time))
-			(fetch-incoming-set gene-pair-pred)
-			(format #t "Elapsed time to load gene pairs: ~A secs\n"
-				(- (current-time) start-time))
-		)
-
-		; Delete the pairs from the atomspace AND the database.
-		; But only those that are currently in the atomspace are
-		; deleted; if any are hiding in the database, they will not be
-		; touched.
-		(define (delete-gene-pairs)
-			(define start-time (current-time))
-			(for-each (lambda (PAIR) (cog-delete-recursive (gdr PAIR)))
-				(cog-incoming-set gene-pair-pred))
-			(cog-delete gene-pair-pred)
-			(cog-delete any-left)
-			(cog-delete any-right)
-			(format #t "Elapsed time to delete gene pairs: ~A secs\n"
-				(- (current-time) start-time))
-		)
-
-		; Methods on the object
-		(lambda (message . args)
-			(apply (case message
-					((name) (lambda () "Gene pairs, predicate `interacts_with`"))
-					((id)   (lambda () "Gene-pairs"))
-					((left-type) get-left-type)
-					((right-type) get-right-type)
-					((pair-type) get-pair-type)
-					((pair-count) get-pair-count)
-					((get-pair) get-pair)
-					((get-count) get-count)
-					((make-pair) make-pair)
-					((left-element) get-left-element)
-					((right-element) get-right-element)
-					((left-wildcard) get-left-wildcard)
-					((right-wildcard) get-right-wildcard)
-					((wild-wild) get-wild-wild)
-					((all-pairs) get-all-pairs)
-					((fetch-pairs) fetch-gene-pairs)
-					((delete-pairs) delete-gene-pairs)
-					((provides) (lambda (symb) #f))
-					((filters?) (lambda () #f))
-					(else (error "Bad method call on gene-pair-api:" message)))
-				args)))
+	(make-evaluation-pair-api
+		(PredicateNode "interacts_with")
+		'GeneNode 'MoleculeNode
+		(AnyNode "left-gene") (AnyNode "right-gene")
+		"Gene-pairs"
+		"Gene pairs, predicate `interacts_with`")
 )
 
 ; ---------------------------------------------------------------------
