980d8fac5c3d5bb6259cd19f278f2c8a4836d345
Linas Vepstas <linasvepstas@gmail.com>
2020-05-18 22:46:09 -0500

    Add tetrhedron code

--- src/bio-tetra.scm
+++ src/bio-tetra.scm
@@ -42,7 +42,7 @@
 
 ;; -----------
 ;; Count triangles.
-(define (count-triangles gene-list)
+(define (count-tetrahedra gene-list)
 	(define bench-secs (make-timer))
 	(define batch-secs (make-timer))
 	(define start-time (get-internal-real-time))
@@ -67,13 +67,19 @@
 					(define gene-a (cog-outgoing-atom gene-triple 0))
 					(define gene-b (cog-outgoing-atom gene-triple 1))
 					(define gene-c (cog-outgoing-atom gene-triple 2))
-					(define act-pair (Evaluation (Predicate "interacts_with") gene-pair))
+					(define pab (Evaluation (Predicate "interacts_with")
+						(List  gene-a gene-b)))
+					(define pbc (Evaluation (Predicate "interacts_with")
+						(List  gene-b gene-c)))
+					(define pca (Evaluation (Predicate "interacts_with")
+						(List  gene-c gene-a)))
 
-xxxxxxxxxxxxxxxxxxxx
-Now what ???
 					(cog-inc-count! gene-a 1)
 					(cog-inc-count! gene-b 1)
-					(cog-inc-count! act-pair 1))
+					(cog-inc-count! gene-c 1)
+					(cog-inc-count! pab 1)
+					(cog-inc-count! pbc 1)
+					(cog-inc-count! pca 1))
 				(cog-outgoing-set result))
 
 			; delete the SetLink
@@ -81,7 +87,7 @@ Now what ???
 			; delete the GetLink, too.
 			(cog-delete query)
 
-			;; (format #t "Ran triangle ~A in ~6f seconds; got ~A results\n"
+			;; (format #t "Ran tetrahedra ~A in ~6f seconds; got ~A results\n"
 			;; 	gene-name (gene-secs) rlen)
 			; (display ".")
 			(set! ndone (+ ndone 1))
@@ -91,25 +97,26 @@ Now what ???
 								internal-time-units-per-second))
 						(rate (/ ndone elapsed)))
 					(format #t
-						"Tri done ~A/~A in ~4f secs rate=~4f gene/sec elapsed=~6f\n"
+						"Tetra done ~A/~A in ~4f secs rate=~4f gene/sec elapsed=~6f\n"
 						ndone ngen (batch-secs) rate elapsed)))
 		)
 		gene-list)
 	(format #t "\n")
-	(format #t "Finished triangle relations for ~A genes in ~8f seconds\n"
+	(format #t "Finished tetrahedron relations for ~A genes in ~8f seconds\n"
 			ngen (bench-secs))
 
 	*unspecified*
 )
 
 ;; -----------
-; Explicitly create and count triangles.
-(define pointed-triangle-query
+; Explicitly create and count tetrahedra.
+(define pointed-tetrahedron-query
 	(Bind
 		(VariableList
 			(TypedVariable (Variable "$a") (Type 'GeneNode))
 			(TypedVariable (Variable "$b") (Type 'GeneNode))
 			(TypedVariable (Variable "$c") (Type 'GeneNode))
+			(TypedVariable (Variable "$d") (Type 'GeneNode))
 		)
 		(And
 			(Evaluation (Predicate "interacts_with")
@@ -118,18 +125,25 @@ Now what ???
 				(List (Variable "$b") (Variable "$c")))
 			(Evaluation (Predicate "interacts_with")
 				(List (Variable "$c") (Variable "$a")))
+			(Evaluation (Predicate "interacts_with")
+				(List (Variable "$a") (Variable "$d")))
+			(Evaluation (Predicate "interacts_with")
+				(List (Variable "$b") (Variable "$d")))
+			(Evaluation (Predicate "interacts_with")
+				(List (Variable "$c") (Variable "$d")))
 		)
-		(Evaluation (Predicate "pointed_triangle")
-			(List (Variable "$a") (Variable "$b") (Variable "$c")))
+		(Evaluation (Predicate "pointed_tetrahedron")
+			(List (Variable "$a") (Variable "$b") (Variable "$c") (Variable "$d")))
 		))
 
 ; Same as above, but not pointed; uses a set.
-(define triangle-query
+(define tetrahedron-query
 	(Bind
 		(VariableList
 			(TypedVariable (Variable "$a") (Type 'GeneNode))
 			(TypedVariable (Variable "$b") (Type 'GeneNode))
 			(TypedVariable (Variable "$c") (Type 'GeneNode))
+			(TypedVariable (Variable "$d") (Type 'GeneNode))
 		)
 		(And
 			(Evaluation (Predicate "interacts_with")
@@ -138,28 +152,34 @@ Now what ???
 				(List (Variable "$b") (Variable "$c")))
 			(Evaluation (Predicate "interacts_with")
 				(List (Variable "$c") (Variable "$a")))
+			(Evaluation (Predicate "interacts_with")
+				(List (Variable "$a") (Variable "$d")))
+			(Evaluation (Predicate "interacts_with")
+				(List (Variable "$b") (Variable "$d")))
+			(Evaluation (Predicate "interacts_with")
+				(List (Variable "$c") (Variable "$d")))
 		)
-		(Evaluation (Predicate "triangle")
-			(Set (Variable "$a") (Variable "$b") (Variable "$c")))
+		(Evaluation (Predicate "tetrahedron")
+			(Set (Variable "$a") (Variable "$b") (Variable "$c") (Variable "$d")))
 		))
 
-(define (make-pointed-triangles)
+(define (make-pointed-tetrahedra)
 	(define elapsed-secs (make-timer))
 	(define pset (cog-execute! pointed-triangle-query))
 	(define points (cog-outgoing-set pset))
 	(define npoints (length points))
 	(cog-delete pset)
-	(format #t "Obtained ~A pointed triangles in ~6f seconds\n"
+	(format #t "Obtained ~A pointed tetrahedra in ~6f seconds\n"
 		npoints (elapsed-secs))
 )
 
-(define (make-triangles)
+(define (make-tetrahedra)
 	(define elapsed-secs (make-timer))
 	(define tset (cog-execute! triangle-query))
-	(define triangles (cog-outgoing-set tset))
-	(define ntris (length triangles))
+	(define tetrahedra (cog-outgoing-set tset))
+	(define ntet (length tetrahedra))
 	(cog-delete tset)
-	(format #t "Obtained ~A triangles in ~6f seconds\n" ntris (elapsed-secs))
+	(format #t "Obtained ~A tetrahedra in ~6f seconds\n" ntet (elapsed-secs))
 )
 
 ; =================================================================
@@ -167,7 +187,7 @@ Now what ???
 
 ; (format #t "AtomSpace contents: ~A\n" (cog-report-counts))
 ;
-; Run the triangle counting code.
-; (count-triangles (cog-get-atoms 'GeneNode))
+; Run the tetrahedra counting code.
+; (count-tetrahedra (cog-get-atoms 'GeneNode))
 
 ; ------------------------------------------------------------------
